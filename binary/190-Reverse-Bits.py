61a959685dace7d6dec1e301728e65d73b6ea620
Abhishek Jana <abhishekjana6@gmail.com>
2020-01-01 07:14:00 -0600

    updated

---
+++ binary/190-Reverse-Bits.py
@@ -0,0 +1,57 @@
+'''
+Reverse bits of a given 32 bits unsigned integer.
+
+ 
+
+Example 1:
+
+Input: 00000010100101000001111010011100
+Output: 00111001011110000010100101000000
+Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+Example 2:
+
+Input: 11111111111111111111111111111101
+Output: 10111111111111111111111111111111
+Explanation: The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293, so return 3221225471 which its binary representation is 10111111111111111111111111111111.
+
+'''
+
+
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        res = 0
+        for i in range(32):
+            res += n & 1
+            n = n >> 1
+            if i != 31:
+                res = res << 1
+        return res
+
+print (Solution().reverseBits(12))
+
+
+
+# Time : O(logn) = O(32)
+# Space: O(1)
+
+class Solution(object):
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        result = 0
+        for i in range(32):
+            result <<= 1
+            result |= n & 1
+            n >>= 1
+        return result
+
+    def reverseBits2(self, n):
+        string = bin(n)
+        if '-' in string:
+            string = string[:3] + string[3:].zfill(32)[::-1]
+        else:
+            string = string[:2] + string[2:].zfill(32)[::-1]
+        return int(string, 2)
+
--- binary/371-Sum-of-Two-Integers.py
+++ binary/371-Sum-of-Two-Integers.py
@@ -10,3 +10,30 @@ Example 2:
 Input: a = -2, b = 3
 Output: 1
 '''
+class Solution:
+    def getSum(self,x, y):
+        import ctypes
+        sum = 0
+        carry = ctypes.c_int32(y)
+        # Iterate till there is no carry
+        while (carry.value != 0):
+            
+            sum = x ^ carry.value
+            # carry now contains common
+            # set bits of x and y
+            carry = ctypes.c_int32(x & carry.value)
+
+            # Sum of bits of x and y where at
+            # least one of the bits is not set
+
+            # Carry is shifted by one so that
+            # adding it to x gives the required sum
+            carry.value <<= 1
+            x = sum
+
+        return sum
+
+res = Solution()
+print(res.getSum(-2, 3))
+
+# Above won't work for negetive
