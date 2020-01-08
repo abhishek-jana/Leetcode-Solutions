4e7fead90d4e4e17d42edc0aef6b89d9d31066f9
Abhishek Jana <abhishekjana6@gmail.com>
2020-01-08 11:59:14 -0600

    editted

--- easy/38-Count-and-Say.py
+++ easy/38-Count-and-Say.py
@@ -14,8 +14,6 @@ Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-
 
 Note: Each term of the sequence of integers will be represented as a string.
 
- 
-
 Example 1:
 
 Input: 1
@@ -27,28 +25,27 @@ Input: 4
 Output: "1211"
 Explanation: For n = 3 the term was "21" in which we have two groups "2" and "1", "2" can be read as "12" which means frequency = 1 and value = 2, the same way "1" is read as "11", so the answer is the concatenation of "12" and "11" which is "1211".
 '''
+
 class Solution:
     def countAndSay(self, n):
         s = "1"
-        for i in range(n-1):
+        for i in range(n - 1):
             s = self.next_number(s)
         return s
-    
-    def next_number(self,s):
+
+    def next_number(self, s):
         result = []
         i = 0
         while i < len(s):
             count = 1
-            while i + 1 < len(s) and s[i] == s[i+1]:
+            while i + 1 < len(s) and s[i] == s[i + 1]:
                 i += 1
                 count += 1
             result.append(str(count) + s[i])
             i += 1
         return ''.join(result)
 
-
-print (Solution().countAndSay(6))
-
+print(Solution().countAndSay(6))
 # Time:  O(n * 2^n)
 # Space: O(2^n)
 # Time:  O(n * 2^n)
@@ -58,7 +55,7 @@ class Solution(object):
     # @return a string
     def countAndSay(self, n):
         seq = "1"
-        for i in xrange(n - 1):
+        for i in range(n - 1):
             seq = self.getNext(seq)
         return seq
 
