22ef5d9c729b5243a9dfb56df0cc2e05d50861b0
Abhishek Jana <abhishekjana6@gmail.com>
2020-01-10 00:57:25 -0600

    updated

--- easy/637-Average-of-Levels-in-Binary-Tree.py
+++ easy/637-Average-of-Levels-in-Binary-Tree.py
@@ -12,5 +12,200 @@ Explanation:
 The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11].
 Note:
 The range of node's value is in the range of 32-bit signed integer.
-
 '''
+'''
+# idea is based on level order traversal using two queues
+"""
+Python program to do level order traversal
+line by line using dual queue"""
+class GFG:
+
+    """Constructor to create a new tree node"""
+    def __init__(self,data):
+        self.val = data
+        self.left = None
+        self.right = None
+
+    """Prints level order traversal line by
+    line using two queues."""
+    def levelOrder(self,node):
+        q1 = [] # Queue 1
+        q2 = [] # Queue 2
+        q1.append(node)
+
+        """Executing loop till both the
+        queues become empty"""
+        while(len(q1) > 0 or len(q2) > 0):
+
+            """Empty string to concatenate
+            the string for q1"""
+            concat_str_q1 = ''
+            while(len(q1) > 0):
+
+                """Poped node at the first
+                pos in queue 1 i.e q1"""
+                poped_node = q1.pop(0)
+                concat_str_q1 += poped_node.val +' '
+
+                """Pushing left child of current
+                node in first queue into second queue"""
+                if poped_node.left:
+                    q2.append(poped_node.left)
+
+                """Pushing right child of current node
+                in first queue into second queue"""
+                if poped_node.right:
+                    q2.append(poped_node.right)
+            print( str(concat_str_q1))
+            concat_str_q1 = ''
+
+            """Empty string to concatenate the
+            string for q1"""
+            concat_str_q2 = ''
+            while (len(q2) > 0):
+
+                """Poped node at the first pos
+                in queue 1 i.e q1"""
+                poped_node = q2.pop(0)
+                concat_str_q2 += poped_node.val + ' '
+
+                """Pushing left child of current node
+                in first queue into first queue"""
+                if poped_node.left:
+                    q1.append(poped_node.left)
+
+                """Pushing right child of current node
+                in first queue into first queue"""
+                if poped_node.right:
+                    q1.append(poped_node.right)
+            print(str(concat_str_q2))
+            concat_str_q2 = ''
+
+""" Driver program to test above functions"""
+node = GFG("1")
+node.left = GFG("2")
+node.right = GFG("3")
+node.left.left = GFG("4")
+node.left.right = GFG("5")
+node.right.right = GFG("6")
+node.levelOrder(node)
+'''
+'''
+# Definition for a binary tree node.
+class Node:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def averageOfLevels(self, root):
+        pass
+
+    def bfs(self,root):
+        arr = []
+        if root.left is None and root.right is None:
+            return
+        left = root.left
+        right = root.right
+        #avg = (left+right)/2
+        return (self.bfs(root.left) + self.bfs(root.right))/2
+
+def printLevelOrder(root):
+    h = height(root)
+    for i in range(1,h+1):
+        printGivenLevel(root,i)
+
+def printGivenLeve(root,level):
+    if root is None:
+        return 0
+    if level == 1:
+        return root.val, level
+    elif level>1:
+        printGivenLevel(root.left,level-1)
+        printGivenLevel(root.right,level-1)
+
+def height(node):
+    if node in None:
+        return 0
+
+    lheight = height(node.left)
+    rheight = height(node.right)
+    if lheight > rheight:
+        return lheight+1
+    else:
+        return rheight+1
+
+
+# Driver program to test above function
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print ("Level order traversal of binary tree is -")
+printLevelOrder(root)
+'''
+'''
+# Recursive Python program for level order traversal of Binary Tree
+
+# A node structure
+class Node:
+
+    # A utility function to create a new node
+    def __init__(self, key):
+        self.data = key
+        self.left = None
+        self.right = None
+
+
+# Function to  print level order traversal of tree
+def printLevelOrder(root):
+    h = height(root)
+    for i in range(1, h+1):
+        printGivenLevel(root, i)
+
+
+# Print nodes at a given level
+def printGivenLevel(root , level):
+    if root is None:
+        return
+    if level == 1:
+        print ("%d" %(root.data)),
+    elif level > 1 :
+        printGivenLevel(root.left , level-1)
+        printGivenLevel(root.right , level-1)
+
+
+""" Compute the height of a tree--the number of nodes
+    along the longest path from the root node down to
+    the farthest leaf node
+"""
+def height(node):
+    if node is None:
+        return 0
+    else :
+        # Compute the height of each subtree
+        lheight = height(node.left)
+        rheight = height(node.right)
+
+        #Use the larger one
+        if lheight > rheight :
+            return lheight+1
+        else:
+            return rheight+1
+
+# Driver program to test above function
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print ("Level order traversal of binary tree is -")
+printLevelOrder(root)
+'''
+# Best Explanation
+
+#http://yueguo1217.com/leetcode-637-average-of-levels-in-binary-tree-easy-75-in-python/
--- easy/680-Valid-Palindrome-II.py
+++ easy/680-Valid-Palindrome-II.py
@@ -12,3 +12,44 @@ Note:
 The string will only contain lowercase characters a-z. The maximum length of the string is 50000.
 '''
 
+class Solution(object):
+    # Actually we can make this solution more general
+    def validPalindrome(self, s):
+        return self.validPalindromeHelper(s, 0, len(s) - 1, 1)
+
+    def validPalindromeHelper(self, s, left, right, budget):
+        #print (budget)
+        # Note that budget can be more than 1
+        while left < len(s) and right >= 0 and left <= right and s[left] == s[right]:
+            left += 1
+            right -= 1
+        if left >= len(s) or right < 0 or left >= right:
+            return True
+        if budget == 0:
+            return False
+        budget -= 1
+        return self.validPalindromeHelper(s, left + 1, right, budget) or self.validPalindromeHelper(s, left, right - 1, budget)
+
+print (Solution().validPalindrome('acacba'))
+
+# if only one letter is there
+# Time O(n)
+# Space O(1)
+class Solution:
+    def validPalindrome(self,s):
+        left = 0
+        right = len(s)-1
+        while left < right:
+            if s[left] != s[right]:
+                return self.isPalindrome(s, left, right-1) or self.isPalindrome(s, left+1, right)
+            left, right = left+1, right-1
+        return True
+
+    def isPalindrome(self,s,left,right):
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left,right = left + 1, right - 1
+        return True
+
+print (Solution().validPalindrome('aba'))
