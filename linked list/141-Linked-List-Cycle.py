26a4c0fb074ede915175f67e7600511a7a8f8a0d
Abhishek Jana <abhishekjana6@gmail.com>
2020-01-14 09:31:09 -0600

    a

--- linked list/141-Linked-List-Cycle.py
+++ linked list/141-Linked-List-Cycle.py
@@ -55,3 +55,41 @@ class Solution:
         return False
 
 # https://www.youtube.com/watch?time_continue=363&v=0DqxhTiVPGM&feature=emb_logo
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head:
+            return
+        else:
+            slow = fast = head
+            # Find middle node
+            while fast and fast.next:
+                slow, fast = slow.next, fast.next.next
+
+            # Split the list and reverse the right half
+            lhead, rhead = head, self.reverse(slow.next)
+            slow.next = None
+
+            # Connect two haves
+            lcur, rcur = lhead, rhead
+            while lcur and rcur:
+                tmp, rcur = rcur, rcur.next
+                tmp.next = lcur.next
+                lcur.next, lcur = tmp, lcur.next
+
+    def reverse(self, head: ListNode) -> ListNode:
+        if not head:
+            return None
+        else:
+            prev, cur = None, head
+            while cur:
+                cur.next, cur, prev = prev, cur.next, cur
+            return prev
