2297451f2f4394eafa5557fc4d63104d6410a04b
Abhishek Jana <abhishekjana6@gmail.com>
2020-01-14 09:12:19 -0600

    u

--- linked list/141-Linked-List-Cycle.py
+++ linked list/141-Linked-List-Cycle.py
@@ -36,32 +36,22 @@ Can you solve it using O(1) (i.e. constant) memory?
 
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
-
         if head == None or head.next == None :
             return False
-
         prev = head
         current = head.next
-
         while current.next != None :
-
 #Using the 2 pointers, current moves 2 step while prev move 1 step
 #So that if it's a circle current will be able to chase and meet with prev
-
             current = current.next.next
             prev = prev.next
-
 #If current and prev met in the chase return True
-
             if current == prev :
                 return True
-
 #When it reached at the end of LinkedList element
 #and next is None return False
-
             if current == None or current.next == None :
                 return False
-
         return False
 
 # https://www.youtube.com/watch?time_continue=363&v=0DqxhTiVPGM&feature=emb_logo
