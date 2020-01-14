9619bdbaf429044761f886cd4bd3202e92b53edb
Abhishek Jana <abhishekjana6@gmail.com>
2020-01-14 07:53:34 -0600

    up

--- array/238_product_of_array_except_self.py
+++ array/238_product_of_array_except_self.py
@@ -13,47 +13,50 @@ Could you solve it with constant space complexity? (The output array does not co
 
 class Solution:
     def productExceptSelf(self, nums):
-        
-        # The length of the input array 
+
+        # The length of the input array
         length = len(nums)
-        
+
         # The left and right arrays as described in the algorithm
         L, R, answer = [0]*length, [0]*length, [0]*length
-        
+
         # L[i] contains the product of all the elements to the left
         # Note: for the element at index '0', there are no elements to the left,
         # so the L[0] would be 1
         L[0] = 1
         for i in range(1, length):
-            
+
             # L[i - 1] already contains the product of elements to the left of 'i - 1'
-            # Simply multiplying it with nums[i - 1] would give the product of all 
+            # Simply multiplying it with nums[i - 1] would give the product of all
             # elements to the left of index 'i'
             L[i] = nums[i - 1] * L[i - 1]
-        
+
         # R[i] contains the product of all the elements to the right
         # Note: for the element at index 'length - 1', there are no elements to the right,
         # so the R[length - 1] would be 1
         R[length - 1] = 1
         for i in reversed(range(length - 1)):
-            
+
             # R[i + 1] already contains the product of elements to the right of 'i + 1'
-            # Simply multiplying it with nums[i + 1] would give the product of all 
+            # Simply multiplying it with nums[i + 1] would give the product of all
             # elements to the right of index 'i'
             R[i] = nums[i + 1] * R[i + 1]
-        
+
         # Constructing the answer array
         for i in range(length):
             # For the first element, R[i] would be product except self
             # For the last element of the array, product except self would be L[i]
             # Else, multiple product of all elements to the left and to the right
             answer[i] = L[i] * R[i]
-        
+
         return answer
 
 '''
-Time complexity : O(N) where N represents the number of elements in the input array. We use one iteration to construct the array L, one to construct the array R and one last to construct the answer array using L and R.
-Space complexity : O(N) used up by the two intermediate arrays that we constructed to keep track of product of elements to the left and right.
+Time complexity : O(N) where N represents the number of elements in the input
+array. We use one iteration to construct the array L, one to construct the
+array R and one last to construct the answer array using L and R.
+Space complexity : O(N) used up by the two intermediate arrays that we
+constructed to keep track of product of elements to the left and right.
 '''
 
 product = Solution()
@@ -95,7 +98,11 @@ class Solution:
         return answer
 
 '''
-Time complexity : O(N) where N represents the number of elements in the input array. We use one iteration to construct the array L, one to update the array answeranswer.
-Space complexity : O(1) since don't use any additional array for our computations. The problem statement mentions that using the answeranswer array doesn't add to the space complexity.
+Time complexity : O(N) where N represents the number of elements in the input
+array. We use one iteration to construct the array L, one to update the array
+answeranswer.
+Space complexity : O(1) since don't use any additional array for our
+computations. The problem statement mentions that using the answeranswer
+array doesn't add to the space complexity.
 
 '''
