b662a43e937d1de2b2ff14d5d5dabbb7992af8fa
Abhishek Jana <48491625+abhishek-jana@users.noreply.github.com>
2020-01-15 10:32:43 -0600

    Update 230-Kth-Smallest-Element-in-a-BST.py

--- tree/230-Kth-Smallest-Element-in-a-BST.py
+++ tree/230-Kth-Smallest-Element-in-a-BST.py
@@ -31,5 +31,7 @@ What if the BST is modified (insert/delete operations) often and you need to fin
 class Solution:
     def kthSmallest(self, root, k):     
         def inorder(r):
-            return inorder(r.left) + [r.val] + inorder(r.right) if r else []
+            return inorder(r.left)\
+            + [r.val] + inorder(r.right)\
+            if r else []
         return inorder(root)[k - 1]
