a19c6819a885e768ae823d60ef4c79ba0e19bf68
Abhishek Jana <48491625+abhishek-jana@users.noreply.github.com>
2020-01-15 07:41:51 -0600

    Update 102-Binary-Tree-Level-Order-Traversal.py

--- tree/102-Binary-Tree-Level-Order-Traversal.py
+++ tree/102-Binary-Tree-Level-Order-Traversal.py
@@ -28,7 +28,7 @@ class Solution(object):
         if not root:
             return
         if len(res) < level + 1:
-            res.append([])
+            res.append([]) # len 1 res = [[]]
         res[level].append(root.val)
         self.dfs(root.left, level + 1, res)
         self.dfs(root.right, level + 1, res)
