cb75fb77d5be974d642dc9d594827bccfae2c0bf
Abhishek Jana <48491625+abhishek-jana@users.noreply.github.com>
2020-01-15 20:37:49 -0600

    Update 253-meeting-Rooms-II.py

--- interval/253-meeting-Rooms-II.py
+++ interval/253-meeting-Rooms-II.py
@@ -37,3 +37,21 @@ class Solution:
                 cnt_rooms -= 1  # Release a room.
                 e += 1
         return min_rooms
+    
+    def minMeetingRooms(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: int
+        """
+        # sort the intervals by start time
+        intervals.sort(key = lambda x: x.start)
+        heap = []
+        for interval in intervals:
+            if heap and interval.start >= heap[0]:
+                # room is already used in last meeting
+                #and continue to use the same room for 
+                #this meeting
+                heapq.heapreplace(heap, interval.end)                
+            else:
+                heapq.heappush(heap, interval.end)                
+        return len(heap)
