68e3898dac2839cfd39434fc6ec1ec8d673284af
Abhishek Jana <48491625+abhishek-jana@users.noreply.github.com>
2020-01-15 18:41:56 -0600

    Update 257-Binary-Tree-Paths.py

--- tree/257-Binary-Tree-Paths.py
+++ tree/257-Binary-Tree-Paths.py
@@ -29,27 +29,18 @@ Explanation: All root-to-leaf paths are: 1->2->5, 1->3
 # Time:  O(n * h)
 # Space: O(h)
 
-
 class Solution(object):
     def binaryTreePaths(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[str]
-        """
         if not root:
             return []
-
         paths = []
-
         curr = ""
         self.dfs(root, curr, paths)
         return paths
-
     def dfs(self, root, curr, paths):
         if root.left is None and root.right is None:
             paths.append(curr + str(root.val))
             return paths
-
         if root.left:
             self.dfs(root.left, curr + str(root.val) + '->', paths)
         if root.right:
@@ -57,31 +48,23 @@ class Solution(object):
 
 # Time:  O(n * h)
 # Space: O(h)
-
-
 class Solution(object):
-    # @param {TreeNode} root
-    # @return {string[]}
     def binaryTreePaths(self, root):
         result, path = [], []
         self.binaryTreePathsRecu(root, path, result)
         return result
-
     def binaryTreePathsRecu(self, node, path, result):
         if node is None:
             return
-
         if node.left is node.right is None:
             ans = ""
             for n in path:
                 ans.append(str(n.val) + "->")
             result.append(ans + str(node.val))
-
         if node.left:
             path.append(node)
             self.binaryTreePathsRecu(node.left, path, result)
             path.pop()
-
         if node.right:
             path.append(node)
             self.binaryTreePathsRecu(node.right, path, result)
