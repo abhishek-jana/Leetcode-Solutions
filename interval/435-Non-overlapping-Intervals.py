9cd5d12b7438c646226a5e174571e3dbf339a179
Abhishek Jana <48491625+abhishek-jana@users.noreply.github.com>
2020-01-22 07:07:13 -0600

    Update 435-Non-overlapping-Intervals.py

--- interval/435-Non-overlapping-Intervals.py
+++ interval/435-Non-overlapping-Intervals.py
@@ -22,11 +22,17 @@ Explanation: You don't need to remove any of the intervals since they're already
 '''
 A classic greedy case: interval scheduling problem.
 
-The heuristic is: always pick the interval with the earliest end time. Then you can get the maximal number of non-overlapping intervals. (or minimal number to remove).
+The heuristic is: always pick the interval with the earliest end time. Then you can get the maximal 
+number of non-overlapping intervals. (or minimal number to remove).
 This is because, the interval with the earliest end time produces the maximal capacity to hold rest intervals.
-E.g. Suppose current earliest end time of the rest intervals is x. Then available time slot left for other intervals is [x:]. If we choose another interval with end time y, then available time slot would be [y:]. Since x ≤ y, there is no way [y:] can hold more intervals then [x:]. Thus, the heuristic holds.
+E.g. Suppose current earliest end time of the rest intervals is x. 
+Then available time slot left for other intervals is [x:]. If we choose another interval with end time y, 
+then available time slot would be [y:]. Since x ≤ y, there is no way [y:] can hold more intervals then [x:].
+Thus, the heuristic holds.
 
-Therefore, we can sort interval by ending time and key track of current earliest end time. Once next interval's start time is earlier than current end time, then we have to remove one interval. Otherwise, we update earliest end time.
+Therefore, we can sort interval by ending time and key track of current earliest end time. 
+Once next interval's start time is earlier than current end time, then we have to remove one interval. 
+Otherwise, we update earliest end time.
 '''
 def eraseOverlapIntervals(intervals):
 	end, cnt = float('-inf'), 0
